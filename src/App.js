010af44680ac360558060648fcf58a03a7a9c35d
marianovungel <vungem88@gmail.com>
2024-10-25 18:33:04 -0300

    last

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 
 function App() {
+
+  const Links = ()=>{
+    window.location.replace("/")
+  }
   return (
     <div className="App">
-      <h1>Acesse o Formulário de Pesquisa de Satisfação!</h1>
-      <button>Acessar Agora...</button>
+      <h1 className="Header">Acesse o Formulário de Pesquisa de Satisfação!</h1>
+      <button className="Buttom" onClick={()=>Links}>Acessar Agora...</button>
     </div>
   );
 }
